43e0109dc1cb884ae3666baab4419776b2697a86
SkayPC <worldskaynet@gmail.com>
2021-03-19 19:53:19 +0700

    connect static files

--- landing/urls.py
+++ landing/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from crm import views
@@ -21,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.first_page),
     path('thanks', views.thanks_page, name = 'thanks_page')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
