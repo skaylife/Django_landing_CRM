a35619d2ffec0672b8f1ac541d270ea8b255f093
SkayPC <worldskaynet@gmail.com>
2021-03-20 17:49:43 +0700

    Обработка ошибок try, except

--- crm/views.py
+++ crm/views.py
@@ -25,17 +25,21 @@ def first_page(request):
     return render(request, './index.html', dict_obj)
 
 def thanks_page(request):
-    name = request.POST['name'] # Без информации в в адресной строке
-    phone = request.POST['phone'] # POST
-    # name = request.GET['name'] # Метод отправки формы с информацие в адресной строке
-    # phone = request.GET['phone'] # GET
-    element = Order(order_name=name, order_phone=phone)
-    element.save()
-    # Отправка сообщения в telegram при переходе
-    sendTelegram(tg_name = name, tg_phone = phone)  # Передача переменных name и phone в функцию sendTelegram, для отправки содержания переменных в telegram и формирования POST запроса
-    # name = request.GET.get('name') # Alt
-    # phone = request.GET.get('phone') # Alt
-    return render(request, './thanks.html', {
-        'name' : name,
+    if request.POST:
+        name = request.POST['name'] # Без информации в в адресной строке
+        phone = request.POST['phone'] # POST
+        # name = request.GET['name'] # Метод отправки формы с информацие в адресной строке
+        # phone = request.GET['phone'] # GET
+        element = Order(order_name=name, order_phone=phone)
+        element.save()
+        # Отправка сообщения в telegram при переходе
+        sendTelegram(tg_name = name, tg_phone = phone)  # Передача переменных name и phone в функцию sendTelegram, для отправки содержания переменных в telegram и формирования POST запроса
+        # name = request.GET.get('name') # Alt
+        # phone = request.GET.get('phone') # Alt
+        return render(request, './thanks.html', {
+            'name' : name,
+        })
 
-    })
+    else:
+        return render(request, './thanks.html')
+
--- telebot/sendMessage.py
+++ telebot/sendMessage.py
@@ -3,39 +3,50 @@ from .models import TeleSettings
 
 
 def sendTelegram(tg_name, tg_phone):
-    settings = TeleSettings.objects.get(pk=1) # Получаем значение из бд
-    # Записываем значение занисанное в settings из бд / токен chat_id & text
-    token = str(settings.tg_token)
-    chat_id = str(settings.tg_chat)
-    text = str(settings.tg_message)
-    api = 'https://api.telegram.org/bot'
-    method = api + token + '/sendmessage'
+    if TeleSettings.objects.get(pk=1):
+        settings = TeleSettings.objects.get(pk=1) # Получаем значение из бд
+        # Записываем значение занисанное в settings из бд / токен chat_id & text
+        token = str(settings.tg_token)
+        chat_id = str(settings.tg_chat)
+        text = str(settings.tg_message)
+        api = 'https://api.telegram.org/bot'
+        method = api + token + '/sendmessage'
 
-    # Для замены значений в сообщении в телеграм Name = {name} Phone = {phone}
-    Name_l = text.find('{') # Поиск первой скобки name | find ищет с начала строки
-    Name_r = text.find('}') # Поиск второй скобки name | find ищет с начала строки
-    Phone_l = text.rfind('{') # Тоже самое и c phone, | rfind ищет с конца строки
-    Phone_r = text.rfind('}') # Тоже самое и c правой скобкой phone, | rfind ищет с конца строки
+        if text.find('{') and text.find('}') and text.rfind('{') and text.rfind('}'): # Проверка на наличе скобок
 
-    # Используем разрение строки на 3 части
-    part_1 = text[0 : Name_l] # Разрезаем до первой перемнной {0 :} и псоле первой переменной { 0 : Name_l }
-    part_2 = text[Name_r + 1 : Phone_l] # + 1 Чтоб продвинуться вперед
-    part_3 = text[Phone_r : -1] # От Phone_r и до последнего символа
+            # # Для замены значений в сообщении в телеграм Name = {name} Phone = {phone}
+            # Name_l = text.find('{') # Поиск первой скобки name | find ищет с начала строки
+            # Name_r = text.find('}') # Поиск второй скобки name | find ищет с начала строки
+            # Phone_l = text.rfind('{') # Тоже самое и c phone, | rfind ищет с конца строки
+            # Phone_r = text.rfind('}') # Тоже самое и c правой скобкой phone, | rfind ищет с конца строки
 
+            # Используем разрение строки на 3 части
+            part_1 = text[0 : text.find('{')] # Разрезаем до первой перемнной {0 :} и псоле первой переменной { 0 : Name_l }
+            part_2 = text[text.find('}') + 1 : text.rfind('{')] # + 1 Чтоб продвинуться вперед
+            part_3 = text[text.rfind('}') : -1] # От Phone_r и до последнего символа
 
+            # Соеденение всех частей и присвоение в переменную
+            text_slise = part_1 + tg_name + part_2 + tg_phone + part_3  # добавление переменных в пустое пространство Name и Phone
 
-    # Соеденение всех частей и присвоение в переменную
-    text_slise = part_1 + tg_name + part_2 + tg_phone + part_3 # добавление переменных в пустое пространство Name и Phone
+        else:
+                text_slise = text
 
-    req = requests.post(method, data={
-        'chat_id': chat_id,
-        'text': text_slise
-    })
+        try:
+            req = requests.post(method, data={
+                'chat_id': chat_id,
+                'text': text_slise
+            })
+        except:
+            pass
 
-#https://api.telegram.org/bot
-# 1780657067:AAHhcfuxU8kb4JDHzG2JGKhzEL_0QAutpHU
-# /sendMessage
-# ?chat_id=
-# -569509289
-# &text=Api-Беседа
+        finally:
+            if req.status_code != 200:
+                print('Ошибка отправки')
+            elif req.status_code == 500:
+                print('Ошибка 500')
+            else:
+                print('Все ок сообщение отправлено')
+    else:
+        pass
+
 
